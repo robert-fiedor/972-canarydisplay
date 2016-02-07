172ee285c97b1b652a3227e42aa366fada114ce5
robert-fiedor <robert.fiedor@kaplan.com>
2016-02-07 11:40:35 -0500

    sjsjsjs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var mongojs = require('mongojs');
+
+var db = mongojs.connect('mongodb://canary:canary1@ds059185.mongolab.com:59185/canaries', ['kanarki'])
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -10,6 +14,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+
+  db.kanarki.insert({yo:1})
+  //mongodb://canary:canary1@ds059185.mongolab.com:59185/canaries
 
   response.render('pages/index',{
     tutu:process.env.ROB_PASS
